7dd9901a76d0f21a2951d9929b83539d4e177b98
Mansher Singh <Manshers-MacBook-Pro.local>
2024-04-27 23:44:06 +0530

    check this latest version

--- Project_CSP/schnorr.py
+++ Project_CSP/schnorr.py
@@ -17,15 +17,15 @@ def createH(p, g):
     h = pow(g, alpha, p)
     return h, alpha
 
+def H(message):
+    return int(hashlib.sha256(message.encode()).hexdigest(), 16)
+    
+
 def keygen():
     p = genPrime()
     g = genG(p)          
     h, alpha = createH(p, g)
-
-    def H(message):
-        return int(hashlib.sha256(message.encode()).hexdigest(), 16) % p
-    
-    public_key = {'g': g, 'G': p, 'h': h, 'H': H}
+    public_key = {'g': g, 'G': p, 'h': h}
     private_key = alpha
 
     return public_key, private_key
@@ -33,20 +33,33 @@ def keygen():
 def sign(message, secret_key, public_key):
     beta = random.randint(1, public_key['G'])
     y = pow(public_key['g'], beta, public_key['G'])
-    c = public_key['H'](str(y) + message)
+    c = H(str(y) + message)
     z = (beta + secret_key * c) % public_key['G']
     return (c, z)
 
-def verify(signature, public_key, message):
+def verify(signature, public_key, message, p):
     c, z = signature
     g = public_key['g']
     h = public_key['h']
-    m_hash = public_key['H'](str(pow(g, z, public_key['G'])) + message)
-    return c == m_hash
+    m_hash = H(str(c) + message)
+    return z == (c + h * m_hash) % p
+
 
 # Example usage
 public_key, private_key = keygen()
+print("Public Key:", public_key)
+print("Private Key:", private_key)
+
 message = "Hello, world!"
+
+# Convert the message to binary
+message = ''.join(format(ord(i), '08b') for i in message)
+
+print("Message:", message)
+
 signature = sign(message, private_key, public_key)
-verification = verify(signature, public_key, message)
+print("Signature:", signature)
+
+verification = verify(signature, public_key, message, public_key['G'])
 print("Signature Verification Result:", verification)
+
